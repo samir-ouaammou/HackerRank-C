7abebc7148915eb25819016e0e7fe318b7a2015b
samir-ouaammou <samirouaammou.4.4.2003@gmail.com>
2025-01-08 19:59:12 +0100

    HackerRank-C

--- ex00.c
+++ ex00.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
-int main() {
-    char s[100];
+int	main(void)
+{
+	char	s[100];
 
-    fgets(s, sizeof(s), stdin);
-    printf("Hello, World!\n");
-    printf("%s", s);
-
-    return 0;
+	fgets(s, sizeof(s), stdin);
+	printf("Hello, World!\n");
+	printf("%s", s);
+	return (0);
 }
